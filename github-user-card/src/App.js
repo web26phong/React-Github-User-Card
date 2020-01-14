a5a0916c47629337523f7825397cd1bfa6f8f35b
Phong Yang <phongyang88@hotmail.com>
2020-01-14 16:21:56 -0600

    installed reactstrap and bootstrap for styling. basic styling done

--- github-user-card/src/App.js
+++ github-user-card/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Row, Col, Card, CardImg, CardSubtitle, CardTitle, CardBody, CardLink} from "reactstrap";
 import axios from "axios";
 import './App.css';
 
@@ -13,9 +14,11 @@ class App extends Component {
     .get(`https://api.github.com/users/PHONGdotTech/followers`)
     .then(res => {
       console.log(res)
+      console.log("set state data followers to the response.data")
       this.setState({
         followers: res.data
       })
+      
     })
     .catch(err => {
       console.log(err)
@@ -27,21 +30,19 @@ class App extends Component {
     
     return (
       <div className="App">
-        <div>
+        <Row className="row">
           {this.state.followers.map(follower => (
-            <div key={follower.id} className="followerContainer">
-              <div className="followerAvatar">
-                <img src={follower.avatar_url} alt="user's avatar"/>
-              </div>
-              <div className="followerDetails">
-                <p>{follower.login}</p>
-                <p><a rel="noopener noreferrer" target="_blank" href={follower.html_url}>{follower.html_url}</a></p>
-              </div>
-            </div>
+            <Card className="card" body inverse color="info" key={follower.id}>
+              <CardImg src={follower.avatar_url} alt="user's avatar"/>
+              <CardBody>
+                <CardTitle>{follower.login}</CardTitle>
+                <CardSubtitle><CardLink style={{color: "black"}} rel="noopener noreferrer" target="_blank" href={follower.html_url}>{follower.html_url}</CardLink></CardSubtitle>
+              </CardBody>
+            </Card>
           ))}
-        </div>
-        
+        </Row>
       </div>
+      
     );
   }
 }
